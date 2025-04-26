b9e8546fb0c6e04b124291d7c8d58833a8b5299e
Jan <janjanpingul@gmail.com>
2025-04-27 02:28:10 +0800

    Props Handling (Advanced)

--- src/App.jsx
+++ src/App.jsx
@@ -40,37 +40,32 @@ const App = () => {
   );
 };
 
-const List = (props) => {
+const List = ({ list }) => {
   return (
     <ul>
-      {props.list.map((item) => (
+      {list.map((item) => (
         <Item key={item.objectID} item={item} />
       ))}
     </ul>
   );
 };
 
-const Item = (props) => (
-  <li key={props.item.objectID}>
+const Item = ({ item }) => (
+  <li key={item.objectID}>
     <span>
-      <a href={props.item.url}>{props.item.title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </li>
 );
 
-const Search = (props) => {
+const Search = ({ onSearch, searchTerm }) => {
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input
-        id="search"
-        type="text"
-        onChange={props.onSearch}
-        value={props.searchTerm}
-      />
+      <input id="search" type="text" onChange={onSearch} value={searchTerm} />
     </div>
   );
 };
