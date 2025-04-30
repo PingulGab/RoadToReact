da273cae2a66b012862e1d6b23736bf294544c9f
Jan <janjanpingul@gmail.com>
2025-04-30 23:51:44 +0800

    Integration Testing: Component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,10 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
-import { Item, SearchForm, storiesReducer } from "./App";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import axios from "axios";
+import { describe, expect, it, Mock, vi } from "vitest";
+import App, { Item, SearchForm, storiesReducer } from "./App";
+
+vi.mock("axios");
+const mockedAxiosGet = axios.get as Mock;
 
 const storyOne = {
   title: "React",
@@ -21,6 +25,112 @@ const storyTwo = {
 };
 
 const stories = [storyOne, storyTwo];
+
+describe("App", () => {
+  it("succeeds fetching data", async () => {
+    const promise = Promise.resolve({
+      data: {
+        hits: stories,
+      },
+    });
+    mockedAxiosGet.mockImplementationOnce(() => promise);
+    render(<App />);
+
+    expect(screen.queryByText(/Loading/)).toBeInTheDocument();
+
+    await waitFor(async () => await promise);
+    expect(screen.queryByText(/Loading/)).toBeNull();
+    expect(screen.getByText("React")).toBeInTheDocument();
+    expect(screen.getByText("Redux")).toBeInTheDocument();
+    expect(screen.getAllByText("Dismiss").length).toBe(2);
+  });
+
+  it("fails fetching data", async () => {
+    const promise = Promise.reject();
+    mockedAxiosGet.mockImplementationOnce(() => promise);
+    render(<App />);
+    expect(screen.getByText(/Loading/)).toBeInTheDocument();
+    try {
+      await waitFor(async () => await promise);
+    } catch (error) {
+      expect(screen.queryByText(/Loading/)).toBeNull();
+      expect(screen.queryByText(/went wrong/)).toBeInTheDocument();
+    }
+  });
+
+  it("removes a story", async () => {
+    const promise = Promise.resolve({
+      data: {
+        hits: stories,
+      },
+    });
+    mockedAxiosGet.mockImplementationOnce(() => promise);
+    render(<App />);
+    await waitFor(async () => await promise);
+    expect(screen.getAllByText("Dismiss").length).toBe(2);
+    expect(screen.getByText("Jordan Walke")).toBeInTheDocument();
+    fireEvent.click(screen.getAllByText("Dismiss")[0]);
+    expect(screen.getAllByText("Dismiss").length).toBe(1);
+    expect(screen.queryByText("Jordan Walke")).toBeNull();
+  });
+
+  it("searches for specific stories", async () => {
+    const reactPromise = Promise.resolve({
+      data: {
+        hits: stories,
+      },
+    });
+    const anotherStory = {
+      title: "JavaScript",
+      url: "https://en.wikipedia.org/wiki/JavaScript",
+      author: "Brendan Eich",
+      num_comments: 15,
+      points: 10,
+      objectID: 3,
+    };
+    const javascriptPromise = Promise.resolve({
+      data: {
+        hits: [anotherStory],
+      },
+    });
+    mockedAxiosGet.mockImplementation((url) => {
+      if (url.includes("React")) {
+        return reactPromise;
+      }
+      if (url.includes("JavaScript")) {
+        return javascriptPromise;
+      }
+      throw Error();
+    });
+
+    // Initial Render
+    render(<App />);
+
+    // First Data Fetching
+    await waitFor(async () => await reactPromise);
+    expect(screen.queryByDisplayValue("React")).toBeInTheDocument();
+    expect(screen.queryByDisplayValue("JavaScript")).toBeNull();
+    expect(screen.queryByText("Jordan Walke")).toBeInTheDocument();
+    expect(screen.queryByText("Dan Abramov, Andrew Clark")).toBeInTheDocument();
+    expect(screen.queryByText("Brendan Eich")).toBeNull();
+
+    // User Interaction-> Search
+    fireEvent.change(screen.queryByDisplayValue("React")!, {
+      target: {
+        value: "JavaScript",
+      },
+    });
+    expect(screen.queryByDisplayValue("React")).toBeNull();
+    expect(screen.queryByDisplayValue("JavaScript")).toBeInTheDocument();
+
+    fireEvent.submit(screen.queryByText("Submit")!);
+    // Second Data Fetching
+    await waitFor(async () => await javascriptPromise);
+    expect(screen.queryByText("Jordan Walke")).toBeNull();
+    expect(screen.queryByText("Dan Abramov, Andrew Clark")).toBeNull();
+    expect(screen.queryByText("Brendan Eich")).toBeInTheDocument();
+  });
+});
 
 describe("storiesReducer", () => {
   it("removes a story from all stories", () => {
