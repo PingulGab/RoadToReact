0c18b770004490b5b5a642055b01334646b44525
Jan <janjanpingul@gmail.com>
2025-04-30 21:22:42 +0800

    CSS in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React from "react";
+import "./App.css";
 
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -91,15 +92,14 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
 
-      <hr />
       {stories.isError && <p>Something went wrong.</p>}
       {stories.isLoading ? (
         <p>Loading...</p>
@@ -111,7 +111,7 @@ const App = () => {
 };
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className="search-form">
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -120,7 +120,11 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
     >
       <strong>Search</strong>
     </InputWithLabel>
-    <button type="submit" disabled={!searchTerm}>
+    <button
+      type="submit"
+      disabled={!searchTerm}
+      className="button button_large"
+    >
       Submit
     </button>
   </form>
@@ -138,15 +142,19 @@ const List = ({ list, onRemoveItem }) => {
 
 const Item = ({ item, onRemoveItem }) => {
   return (
-    <li key={item.objectID}>
-      <span>
+    <li className="item">
+      <span style={{ width: "40%" }}>
         <a href={item.url}>{item.title}</a>
       </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
-        <button type="button" onClick={() => onRemoveItem(item)}>
+      <span style={{ width: "30%" }}>{item.author}</span>
+      <span style={{ width: "10%" }}>{item.num_comments}</span>
+      <span style={{ width: "10%" }}>{item.points}</span>
+      <span style={{ width: "10%" }}>
+        <button
+          type="button"
+          onClick={() => onRemoveItem(item)}
+          className="button button_small"
+        >
           Dismiss
         </button>
       </span>
@@ -172,7 +180,10 @@ const InputWithLabel = ({
 
   return (
     <>
-      <label htmlFor={id}> {children} </label>
+      <label htmlFor={id} className="label">
+        {" "}
+        {children}{" "}
+      </label>
       &nbsp;
       <input
         id={id}
@@ -180,6 +191,7 @@ const InputWithLabel = ({
         value={value}
         onChange={onInputChange}
         ref={inputRef}
+        className="input"
       />
     </>
   );
