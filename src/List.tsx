1609961f8d216a46f4257222d4a9fefe7dc45f44
Jan <janjanpingul@gmail.com>
2025-05-01 00:25:05 +0800

    Real World React: Reverse Sorting

--- src/List.tsx
+++ src/List.tsx
@@ -23,14 +23,17 @@ const SORTS: Record<string, SortFunction> = {
 };
 
 const List = ({ list, onRemoveItem }: ListProps) => {
-  const [sort, setSort] = React.useState("NONE");
+  const [sort, setSort] = React.useState({ sortKey: "NONE", isReverse: false });
 
   const handleSort = (sortKey: string) => {
-    setSort(sortKey);
+    const isReverse = sort.sortKey === sortKey && !sort.isReverse;
+    setSort({ sortKey, isReverse });
   };
 
-  const sortFunction = SORTS[sort];
-  const sortedList = sortFunction(list);
+  const sortFunction = SORTS[sort.sortKey];
+  const sortedList = sort.isReverse
+    ? sortFunction(list).reverse()
+    : sortFunction(list);
   return (
     <ul>
       <li style={{ display: "flex" }}>
